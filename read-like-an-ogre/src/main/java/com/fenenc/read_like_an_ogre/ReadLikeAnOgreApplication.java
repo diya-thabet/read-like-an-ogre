67e23cdc73b47b37e6ae259301aa5c4975ec5763
diya-thabet <mohamaddhia@gmail.com>
2025-08-04 20:15:55 +0100

    user + token + role

--- read-like-an-ogre/src/main/java/com/fenenc/read_like_an_ogre/ReadLikeAnOgreApplication.java
+++ read-like-an-ogre/src/main/java/com/fenenc/read_like_an_ogre/ReadLikeAnOgreApplication.java
@@ -2,8 +2,10 @@ package com.fenenc.read_like_an_ogre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReadLikeAnOgreApplication {
 
 	public static void main(String[] args) {
---
+++ read-like-an-ogre/src/main/java/com/fenenc/read_like_an_ogre/user/Token.java
@@ -0,0 +1,27 @@
+package com.fenenc.read_like_an_ogre.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String Token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
